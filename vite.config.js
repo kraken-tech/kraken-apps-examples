f4cfded7eee3056558ed32c1b28fe3a82a760f42
riccardoNovaglia <riccardo.novaglia+github@gmail.com>
2025-05-22 15:30:50 +0100

    Update the example Vite config to produce a single JS bundle

--- vite.config.js
+++ vite.config.js
@@ -4,11 +4,24 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
-
   return {
     plugins: [react()],
     server: {
       cors: mode === "development",
     },
+    build: {
+      rollupOptions: {
+        output: {
+          // Avoid chunk splitting for the main entry point.
+          manualChunks: undefined,
+          // Avoid chunk splitting for dynamic imports.
+          inlineDynamicImports: true,
+        },
+      },
+      // Massively increase the inline limit to prevent bundle splitting.
+      assetsInlineLimit: 100000000,
+      // Prevent CSS splitting.
+      cssCodeSplit: false,
+    }
   };
 });
