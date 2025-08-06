2cdcdba5248096eac892f659c626258860bac7c9
riccardoNovaglia <riccardo.novaglia+github@gmail.com>
2025-08-06 16:00:56 +0100

    Create a Debug App which can be used to investigate apps args
    As it can be very useful to debug things locally and understand how apps
    work.

---
+++ src/components/DebugApp.tsx
@@ -0,0 +1,37 @@
+import { useLocation } from "react-router";
+import { useConfig } from "./Providers/Config";
+
+export const DebugApp = ({ props }: { props: any }) => {
+  const config = useConfig();
+  console.log(props);
+
+  const { pathname, search } = useLocation();
+  return (
+    <>
+      <p>
+        My URL is <code>{window.location.href}</code>
+      </p>
+      <p>
+        It might be different quite from the browser URL, since I'm rendered
+        inside an iframe!
+      </p>
+      <p>
+        Inside my Router, my basename is "<code>{config.basename}</code>". I
+        will only render content if my URL starts with that.
+      </p>
+      <p>
+        My base link is "<code>{config.linkBaseRoute}</code>". All links inside
+        my app should start with that to make sure I'm rendered, and anything
+        following that is passed to me.
+      </p>
+      <p>
+        I'm currently at "{pathname}", with query params "{search}".
+      </p>
+      <p>
+        These are your user's details:
+        <pre>{JSON.stringify(props.user, null, 2)}</pre>
+      </p>
+      <p>All props have been logged to the console!</p>
+    </>
+  );
+};
